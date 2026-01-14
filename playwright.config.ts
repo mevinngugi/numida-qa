312953f6e0cdcb4a5aa1f2b34098acf8b3f7c16d
Mevin Ngugi <16956310+mevinngugi@users.noreply.github.com>
2026-01-14 10:54:53 +0300

    Install allure command line, update reporters, enable CI to generate allure reports

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,8 +22,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  //reporter: 'html',
-  reporter: [["line"], ["allure-playwright"]],
+  // When in CI use allure but on local use the list reporter
+  reporter: process.env.CI ? 'allure-playwright' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
